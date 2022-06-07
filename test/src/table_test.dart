5cf257a6e98fdf3333a7bb0c112ad4602ddcc843
Brad Cypert <brad.cypert@gmail.com>
2022-06-07 12:48:11 -0400

    add a few tests for tables

--- test/src/table_test.dart
+++ test/src/table_test.dart
@@ -11,11 +11,15 @@ class Mock extends SchemaObject {
     };
   }
 
+  Mock();
+
   // normally this would set properties on the object from the map
   Mock.fromMap(Map<String, dynamic> map);
 }
 
 class FakeAdapter extends Adapter {
+  List<Map<String, dynamic>> _data = [];
+
   @override
   Future<void> close() {
     throw UnimplementedError();
@@ -28,7 +32,8 @@ class FakeAdapter extends Adapter {
 
   @override
   Future<void> insert(Insert<SchemaObject> insert) {
-    throw UnimplementedError();
+    _data.add(insert.item.toMap());
+    return Future.value();
   }
 
   @override
@@ -56,9 +61,10 @@ class FakeAdapter extends Adapter {
 }
 
 class MockTable extends Table<Mock> {
+  final Adapter _adapter = FakeAdapter();
   MockTable() : super(name: 'Mock');
   @override
-  Adapter get adapter => FakeAdapter();
+  Adapter get adapter => _adapter;
 
   static final UUID = Column('uuid', primaryKey: true);
 }
@@ -76,6 +82,52 @@ void main() {
       test('should return a single item given a primary key', () async {
         final table = MockTable();
         expect((await table.findByPk(1)).id, equals(1));
+      });
+    });
+
+    group('insert', () {
+      test('should call the adapter with an insert query', () async {
+        final table = MockTable();
+        await table.insert(Mock());
+        expect((table.adapter as FakeAdapter)._data, isNotEmpty);
+      });
+    });
+
+    group('insert', () {
+      test('should call the adapter with an insert query', () async {
+        final table = MockTable();
+        await table.insert(Mock());
+        expect((table.adapter as FakeAdapter)._data, isNotEmpty);
+      });
+    });
+
+    group('delete', () {
+      test('should return a delete query', () async {
+        final table = MockTable();
+        final delete = table.delete();
+        expect(delete, isA<Delete>());
+        expect(delete.clauses, isEmpty);
+      });
+
+      test(
+          'should return a delete query already configured for primary key lookup when an item is provided',
+          () async {
+        final table = MockTable();
+        final delete = table.delete(Mock());
+        expect(delete, isA<Delete>());
+        expect(delete.clauses, isNotEmpty);
+      });
+    });
+
+    group('where', () {
+      test('should return a where query with a where clause', () async {
+        final table = MockTable();
+        final where = table.where(MockTable.UUID, '=', 1);
+        expect(where, isA<Query>());
+        expect(where.clauses, isNotEmpty);
+        expect(where.clauses.first.col, equals(MockTable.UUID));
+        expect(where.clauses.first.comparator, equals('='));
+        expect(where.clauses.first.value, equals(1));
       });
     });
   });
